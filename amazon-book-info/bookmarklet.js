ac7de9da697d7e8629942092a8dd6011d2d73801
seking <seking@users.noreply.github.com>
2026-03-09 01:33:24 +0900

    Create bookmarklet.js

---
+++ amazon-book-info/bookmarklet.js
@@ -0,0 +1,2 @@
+// use this script for boookmarklet
+javascript:(function(d,s){s=d.createElement('script');s.src='https://seking.github.io/bookmarklet/amazon-book-info/script.js';d.body.appendChild(s);})(document) 
